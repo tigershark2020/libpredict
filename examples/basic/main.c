e39e19f3f3b364cbfa32a785fc8d7fdcb2360b58
tigershark2020 <64547173+tigershark2020@users.noreply.github.com>
2024-03-29 00:47:29 -0500

    Update main.c

--- examples/basic/main.c
+++ examples/basic/main.c
@@ -2,8 +2,59 @@
 #include <math.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <curl/curl.h>
 #include <predict/predict.h>
+
+void pushoverNotifySolarEclipse()
+{
+  CURL *curl;
+  CURLcode res;
+
+  /* In windows, this will init the winsock stuff */
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  /* get a curl handle */
+  curl = curl_easy_init();
+  if(curl) {
+    /* First set the URL that is about to receive our POST. This URL can
+       just as well be a https:// URL if that is what should receive the
+       data. */
+    curl_easy_setopt(curl, CURLOPT_URL, "https://api.pushover.net/1/messages.json");
+    /* Now specify the POST data */
+
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "content-type: application/x-www-form-urlencoded");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+    char description[48];
+		strcpy(description,"NEARING SOLAR ECLIPSE");
+
+    char postfields[300];
+
+    int q;
+
+    char unescaped_message[200];
+    snprintf(unescaped_message,200,"%s", description);
+    char *message = curl_easy_escape(curl, unescaped_message,0);
+    q=snprintf(postfields, 300, "token={pushover_api_key}&user={pushover_user_key}&message=%s",message);
+
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postfields);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1);
+
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+  curl_global_cleanup();
+
+}
+
 
 int main(int argc, char **argv)
 {
@@ -19,7 +70,7 @@ int main(int argc, char **argv)
 	}
 
 	// Create observer object
-	predict_observer_t *obs = predict_create_observer("Me", 63.9*M_PI/180.0, 10.9*M_PI/180.0, 0);
+	predict_observer_t *obs = predict_create_observer("Me", 41.6191*M_PI/180.0, -83.5807*M_PI/180.0, 0);
 	if (!obs) {
 		fprintf(stderr, "Failed to initialize observer!");
 		exit(1);
@@ -49,12 +100,28 @@ int main(int argc, char **argv)
 		// Predict and observe MOON
 		struct predict_observation moon_obs;
 		predict_observe_moon(obs, curr_time, &moon_obs);
-		printf("MOON: %f, %f\n", moon_obs.azimuth*180.0/M_PI, moon_obs.elevation*180.0/M_PI);
+		double moon_azimuth_float = moon_obs.azimuth*180.0/M_PI;
+		double moon_elevation_float = moon_obs.elevation*180.0/M_PI;
+		printf("MOON: %f, %f\n", moon_azimuth_float , moon_elevation_float);
 
 		// Predict and observe SUN
 		struct predict_observation sun_obs;
 		predict_observe_sun(obs, curr_time, &sun_obs);
-		printf("SUN: %f, %f\n", sun_obs.azimuth*180.0/M_PI, sun_obs.elevation*180.0/M_PI);
+		double sun_azimuth_float = sun_obs.azimuth*180.0/M_PI;
+		double sun_elevation_float = sun_obs.elevation*180.0/M_PI;
+		printf("SUN: %f, %f\n", sun_azimuth_float, sun_elevation_float);
+
+		double eclipse_azimuth_threshold = (moon_azimuth_float > sun_azimuth_float) ? (moon_azimuth_float - sun_azimuth_float) : (sun_azimuth_float - moon_azimuth_float);
+		double eclipse_elevation_threshold = (moon_elevation_float > sun_elevation_float) ? (moon_elevation_float - sun_elevation_float) : (sun_elevation_float - moon_elevation_float);
+
+		printf("SOLAR ELCIPSE THRESHOLD: %f, %f\n", eclipse_azimuth_threshold , eclipse_elevation_threshold);
+
+		if(eclipse_azimuth_threshold < 1 && eclipse_elevation_threshold < 1)
+		{
+			pushoverNotifySolarEclipse();
+			printf("SOLAR:\tNEARING SOLAR ECLIPSE");
+			exit(0);
+		}
 
 		//Sleep
 		fflush(stdout);
